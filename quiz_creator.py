e4867b1cae25bcea355b1a56fa4cec065150a032
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-04-08 16:56:19 +0800

    added the inputs for the question making function and added the pseudocode for saving the question data

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,8 +1,21 @@
-# defining the function of creating a question
+# defining the function for creating a question
+def question_making():
     # input the question
+    question = input("Enter the question\n")
     # input the choices
+    choices = {}
+    for option in ['A', 'B', 'C', 'D']:
+        choices[option] = input(f"Enter the answer for option {option}\n")
     # input the correct answer
+    correct_answer = input("Enter the correct answer for this question\n")
+    # save the question data using a dictionary
 # using a loop to create question and stopping when prompted to
     # run the creating a question function
     # ask if the user to continue
 # export the data to a text file
+
+
+
+
+
+question_making()
