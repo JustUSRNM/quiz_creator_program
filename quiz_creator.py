4d4c19a984ead1a130ec0dcbe50a89e1532c5742
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-04-08 17:18:07 +0800

    added a way to save the data of a question and a checker to make sure the correct answers is part of the choices

--- quiz_creator.py
+++ quiz_creator.py
@@ -8,7 +8,11 @@ def question_making():
         choices[option] = input(f"Enter the answer for option {option}\n")
     # input the correct answer
     correct_answer = input("Enter the correct answer for this question\n")
+    while correct_answer not in choices.values():
+        correct_answer = input("Your correct answer is not in the choices you have given\nThe correct anwers must be one of your choices\nenter the correct answer again\n")
     # save the question data using a dictionary
+    question_data = {'question' : question, 'choices' : choices, 'answer' : correct_answer}
+    return question_data
 # using a loop to create question and stopping when prompted to
     # run the creating a question function
     # ask if the user to continue
