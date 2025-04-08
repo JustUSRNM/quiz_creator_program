c3d9a7063af997c04dca11c2b7f381893c0d1342
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-04-08 16:39:51 +0800

    created the main file + added the pseudocode

---
+++ quiz_creator.py
@@ -0,0 +1,8 @@
+# defining the function of creating a question
+    # input the question
+    # input the choices
+    # input the correct answer
+# using a loop to create question and stopping when prompted to
+    # run the creating a question function
+    # ask if the user to continue
+# export the data to a text file
