d167707fdb7ff7e887d36b038a993049093b457e
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-05-09 02:58:32 +0800

    added a random question picker

--- quiz_taker.py
+++ quiz_taker.py
@@ -10,8 +10,10 @@ filename = os.path.join(program_path, "quiz.json")
 # open the file
 with open(filename, 'r') as file:
     quiz_data = json.load(file)
-print (quiz_data)
+
 # pick a random question
+random_question = random.choice(quiz_data)
+print (random_question)
 # let the taker answer the question
 # check the answer if it is correct
 # display if they passed or not
